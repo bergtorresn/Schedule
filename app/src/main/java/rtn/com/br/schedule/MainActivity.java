1f172e2b04f1ede1f771af54d9670de6c05b73b9
Rosemberg Torres Nunes <bergtorres@Mac-mini-de-Rosemberg-2.local>
2018-06-16 20:16:35 -0300

    firebase-auth:16.0.2

--- app/src/main/java/rtn/com/br/schedule/MainActivity.java
+++ app/src/main/java/rtn/com/br/schedule/MainActivity.java
@@ -3,7 +3,9 @@ package rtn.com.br.schedule;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
