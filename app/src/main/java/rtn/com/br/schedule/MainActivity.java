231ab44edf80110878bc4c5ed7e4c72493c4a833
Rosemberg Torres Nunes <bergtorres@Mac-mini-de-Rosemberg-2.local>
2018-06-16 15:57:45 -0300

    primeiro commit

---
+++ app/src/main/java/rtn/com/br/schedule/MainActivity.java
@@ -0,0 +1,13 @@
+package rtn.com.br.schedule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
